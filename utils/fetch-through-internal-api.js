48680efc734a6f297d998d09243f5be3912a7d98
Niklas Rossmann <login+github@niklasrossmann.com>
2020-09-24 23:30:13 +0200

    Remove static base URLs and fetch relative paths instead

--- utils/fetch-through-internal-api.js
+++ utils/fetch-through-internal-api.js
@@ -1,8 +1,6 @@
 /* global fetch */
-const INTERNAL_API_BASE_URL = 'http://localhost:3000/api/laterpay'
-
 const fetchThroughInternalApi = async (url) => {
-  const response = await fetch(`${INTERNAL_API_BASE_URL}${url}`)
+  const response = await fetch(`/api/laterpay${url}`)
   const data = await response.json()
   return data
 }
