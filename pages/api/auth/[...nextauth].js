0a7f2341de344853a59b50f0dc8a5fe7f9bb5c01
Niklas Rossmann <login+github@niklasrossmann.com>
2021-01-23 00:43:16 +0100

    Disable next-auth debugging

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from 'next-auth'
 
 // See https://next-auth.js.org/configuration/options
 const options = {
-  debug: true,
+  // debug: true,
   providers: [
     // Configure custom OAuth2 provider
     {
