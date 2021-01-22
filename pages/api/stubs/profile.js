4d69edfaaccab57f09c3d3f7ebe2ac07ce313ed9
Niklas Rossmann <login+github@niklasrossmann.com>
2021-01-22 22:25:07 +0100

    Disable token decoding and return random id instead

--- pages/api/stubs/profile.js
+++ pages/api/stubs/profile.js
@@ -2,21 +2,21 @@
 // This API page serves as a temporary placeholder.
 export default (req, res) => {
   try {
-    const authHeader = req.headers.authorization
-    if (!authHeader) throw new Error('missing auth header')
-    // Extract Laterpay User ID from access token
-    const accessToken = authHeader.split('Bearer ')[1]
-    const tokenPayload = accessToken.split('.')[1]
-    const buff = Buffer.from(tokenPayload, 'base64')
-    const text = buff.toString('ascii')
-    const tokenData = JSON.parse(text)
-    const laterpayUserId = tokenData.sub
-    // const id = nanoid()
+    // const authHeader = req.headers.authorization
+    // if (!authHeader) throw new Error('missing auth header')
+    // // Extract Laterpay User ID from access token
+    // const accessToken = authHeader.split('Bearer ')[1]
+    // const tokenPayload = accessToken.split('.')[1]
+    // const buff = Buffer.from(tokenPayload, 'base64')
+    // const text = buff.toString('ascii')
+    // const tokenData = JSON.parse(text)
+    // const laterpayUserId = tokenData.sub
     res.status(200).json({
-      id: laterpayUserId,
-      name: `John ${laterpayUserId}`,
-      email: `${laterpayUserId}@user.com`,
-      picture: `https://i.pravatar.cc/150?${laterpayUserId}`
+      id: Math.round(Math.random() * 10000)
+      // id: laterpayUserId,
+      // name: `John ${laterpayUserId}`,
+      // email: `${laterpayUserId}@user.com`,
+      // picture: `https://i.pravatar.cc/150?${laterpayUserId}`
     })
   } catch (error) {
     res.status(500).json({ error: error.message })
