61d005957eeb5db2bfef8eadb10e6b8e9343a2b6
Niklas Rossmann <login+github@niklasrossmann.com>
2021-01-24 14:47:11 +0100

    Use light build of SyntaxHighlighter

--- components/JsonViewer.js
+++ components/JsonViewer.js
@@ -13,8 +13,11 @@ import {
   useDisclosure
 } from '@chakra-ui/react'
 import { Search2Icon } from '@chakra-ui/icons'
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import { docco } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter'
+import json from 'react-syntax-highlighter/dist/esm/languages/hljs/json'
+import docco from 'react-syntax-highlighter/dist/esm/styles/hljs/docco'
+
+SyntaxHighlighter.registerLanguage('json', json)
 
 const JsonViewer = ({ tabData }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -53,13 +56,17 @@ const JsonViewer = ({ tabData }) => {
           <DrawerContent>
             <DrawerCloseButton size='lg' />
             <DrawerHeader>Current Tab</DrawerHeader>
-            <DrawerBody>
+            <DrawerBody pb={6}>
               <Box
                 bg='white'
                 borderRadius='md'
                 overflow='hidden'
               >
-                <SyntaxHighlighter language='json' style={docco}>
+                <SyntaxHighlighter
+                  language='json'
+                  style={docco}
+                  customStyle={{ minHeight: 'calc(100vh - 6rem)' }}
+                >
                   {JSON.stringify(tabData, null, ' ') /* creates JSON string with line breaks */}
                 </SyntaxHighlighter>
               </Box>
